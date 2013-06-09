75db81e826784fff27e514053dffdbc9d819d974
yamashita <yamashita@mk.ces.kyutech.ac.jp>
2013-06-09 19:36:39 +0900

    Added codes to display button.

--- src/main/java/pianoproject/NotesDisplay.java
+++ src/main/java/pianoproject/NotesDisplay.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Container;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -23,9 +24,10 @@ public class NotesDisplay {
   public NotesDisplay() {
     this.frame = new JFrame("ピアノ練習アプリ"); //$NON-NLS-1$
     this.frame.setVisible(true);
-    this.frame.setSize(200, 200);
+    this.frame.setSize(600, 500);
     this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setNotesDisplay();
+    setButton();
   }
 
   private void setNotesDisplay() {
@@ -34,7 +36,33 @@ public class NotesDisplay {
     JLabel label = new JLabel(notes);
     p.add(label);
     Container con = this.frame.getContentPane();
-    con.add(p,BorderLayout.CENTER);
+    con.add(p, BorderLayout.CENTER);
+  }
+
+  private void setButton() {
+    JPanel p = createButton();
+    Container con = this.frame.getContentPane();
+    con.add(p, BorderLayout.SOUTH);
+    
+  }
+
+  private static JPanel createButton() {
+    JPanel p = new JPanel();
+    JButton noteDo = new JButton("ド"); //$NON-NLS-1$
+    JButton noteRe = new JButton("レ"); //$NON-NLS-1$
+    JButton noteMi = new JButton("ミ"); //$NON-NLS-1$
+    JButton noteFa = new JButton("ファ"); //$NON-NLS-1$
+    JButton noteSo = new JButton("ソ"); //$NON-NLS-1$
+    JButton noteRa = new JButton("ラ"); //$NON-NLS-1$
+    JButton noteShi = new JButton("シ"); //$NON-NLS-1$
+    p.add(noteDo);
+    p.add(noteRe);
+    p.add(noteMi);
+    p.add(noteFa);
+    p.add(noteSo);
+    p.add(noteRa);
+    p.add(noteShi);
+    return p;
   }
 
   public void showNotes() {
